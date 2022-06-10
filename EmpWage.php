2a79a69e18feaae27538ffe3eedcf80647cf4307
mabbuprem <mabbupremkumar@gmail.com>
2022-06-10 09:31:51 +0530

    [prem] FEATURE ADDED: Employee dail wage

--- EmpWage.php
+++ EmpWage.php
@@ -2,6 +2,8 @@
 
 class EmployeeWage
 {
+    public $WAGE_PER_HR = 20;
+    public $FULL_TIME_WORKING_HRS = 8;
 
     /**
      * Function to Check Employee is Present or Absent
@@ -11,12 +13,25 @@ class EmployeeWage
     {
         $random = rand(0, 1);
         if ($random == 1) {
-            echo "Employee is Present";
+            echo "Employee is Present \n";
+            return $this->FULL_TIME_WORKING_HRS;
         } else {
-            echo "Employee is Absent";
+            echo "Employee is Absent\n";
+            return 0;
         }
+    }
+
+    /**
+     * Function to Calculate Daily Wage of
+     * employee if he/she is present.
+     */
+    function dailyWage()
+    {
+        $hrs = $this->empAttendance();
+        $dailyWage = $this->WAGE_PER_HR * $hrs;
+        echo "Daily Wage of Employee: " . $dailyWage;
     }
 }
 
 $emp = new EmployeeWage();
-$emp->empAttendance();
+$emp->dailyWage();
