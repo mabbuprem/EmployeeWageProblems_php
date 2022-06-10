2eaf57f9e7dfd115792ecf1c920c1d0d2dc7358f
mabbuprem <mabbupremkumar@gmail.com>
2022-06-10 09:56:23 +0530

    [prem] FEATURE ADDED: By Using Methods

--- EmpWage.php
+++ EmpWage.php
@@ -1,15 +1,15 @@
 <?php
 
-class EmployeeWage
+class Employee_Wage
 {
-    public $WAGE_PER_HR = 20;
-    public $FULL_TIME_WORKING_HRS = 8;
-    public $PART_TIME_WORKING_HRS = 4;
-    public $WORKING_DAYS_PER_MONTH = 20;
-    public $WORKING_HOURS_PER_MONTH = 100;
-    public $COUNT_FULL_TIME=0;
-    public $COUNT_PART_TIME=0;
-    public $COUNT_ABSENT=0;
+    public const WAGE_PER_HR = 20;
+    public const FULL_TIME_WORKING_HRS = 8;
+    public const PART_TIME_WORKING_HRS = 4;
+    public const IS_FULL_TIME = 2;
+    public const IS_PART_TIME = 1;
+    public const IS_ABSENT = 0;
+    public const WORKING_DAYS_PER_MONTH = 20;
+    public const WORKING_HOURS_PER_MONTH = 100;
 
     public $workingHrs = 0;
     public $monthlyWage = 0;
@@ -19,23 +19,24 @@ class EmployeeWage
     /**
      * Function to Check Employee is Present or Absent
      * Returns working hrs
+     * Non-Parameterized Function
      */
-    function empAttendance()
+    function attendance()
     {
         $empCheck = rand(0, 2);
         switch ($empCheck) {
-            case 1:
-                $this->COUNT_PART_TIME++;
+            case $this->IS_PART_TIME:
+                echo "Part Time Employee\n";
                 return $this->PART_TIME_WORKING_HRS;
                 break;
 
-            case 2:
-                $this->COUNT_FULL_TIME++;
+            case $this->IS_FILL_TIME:
+                echo "Full Time Employee\n";
                 return $this->FULL_TIME_WORKING_HRS;
                 break;
 
             default:
-                $this->COUNT_ABSENT++;
+                echo "Employee is Absent\n";
                 return 0;
                 break;
         }
@@ -43,43 +44,43 @@ class EmployeeWage
 
     /**
      * Function to Calculate Daily Wage
+     * Non-Parameterized Function
      * Printing the daily wage to the output
      * Calling attendance function to check employee attendance
      * returns daily wage of the employee
      */
     function dailyWage()
     {
-        $hrs = $this->empAttendance();
-        $this->workingHrs = $hrs;
-        $dailyWage = $this->WAGE_PER_HR * $hrs;
+        $this->workingHrs = $this->attendance();
+        $dailyWage = $this->WAGE_PER_HR * $this->workingHrs;
+        echo "Working Hours:: " . $this->workingHrs . "\n";
+        echo "Daily Wage:: " . $dailyWage . "\n\n";
         return $dailyWage;
     }
 
     /**
      * Function to Calculate Monthly Wage
+     * Non-Parameterized Function
      * Printing the Monthly wage to the output
      * Calling daily wage function to get daily wage
      */
     function monthlyWage()
     {
         while (
-            $this->totalWorkingHours <= $this->WORKING_HOURS_PER_MONTH-4 ||
+            $this->totalWorkingHours <= $this->WORKING_HOURS_PER_MONTH &&
             $this->totalWorkingDays < $this->WORKING_DAYS_PER_MONTH
         ) {
             $this->totalWorkingDays++;
+            echo "Day:: " . $this->totalWorkingDays . "\n";
             $dailyWage = $this->dailyWage();
-
             $this->monthlyWage += $dailyWage;
             $this->totalWorkingHours += $this->workingHrs;
         }
 
-        echo "Total Working Days:: " . $this->COUNT_FULL_TIME + $this->COUNT_PART_TIME . "\n";
+        echo "Total Working Days:: " . $this->totalWorkingDays . "\n";
         echo "Total Working Hours:: " . $this->totalWorkingHours . "\n";
-        echo "Monthly Wage:: " . $this->monthlyWage . "\n";
-        echo "Days Employee was full time in month: " . $this->COUNT_FULL_TIME . "\n";
-        echo "Days Employee was Part time in month: " . $this->COUNT_PART_TIME . "\n";
-        echo "Days Employee was Absent in month: " . $this->COUNT_ABSENT . "\n";
+        echo "Monthly Wage:: " . $this->monthlyWage . "\n\n";
     }
 }
-$employeeWage = new EmployeeWage();
+$employeeWage = new Employee_Wage();
 $employeeWage->monthlyWage();
