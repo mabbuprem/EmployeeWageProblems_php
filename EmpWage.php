7121fbb398df5946878d31d1542744fb9cc23495
mabbuprem <mabbupremkumar@gmail.com>
2022-06-10 09:28:27 +0530

    [prem] FEATURE ADDED: Employee Attendance

--- EmpWage.php
+++ EmpWage.php
@@ -1,8 +1,22 @@
 <?php
-class Employee_Wage {
-    function welcomeMsg()
+
+class EmployeeWage
+{
+
+    /**
+     * Function to Check Employee is Present or Absent
+     * Using rand() to generate the random of 0 or 1
+     */
+    function empAttendance()
     {
-        echo "Welcome to employee wage problem" . "\n";
+        $random = rand(0, 1);
+        if ($random == 1) {
+            echo "Employee is Present";
+        } else {
+            echo "Employee is Absent";
+        }
     }
 }
-?>
+
+$emp = new EmployeeWage();
+$emp->empAttendance();
